5adde9cc7e33070fab9a0dfeab7f8bf8998932cf
meist <stephan.meissner@dccs.at>
2014-01-30 15:59:38 +0100

    + Downsampler skizziert.

--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package main
 
 import (
   "log"
+  "time"
   "flag"
+  "math"
   "strconv"
   "net/http"
   "regexp"
@@ -10,6 +12,8 @@ import (
   _ "github.com/lib/pq"
   "github.com/codegangsta/martini"
   "github.com/codegangsta/martini-contrib/binding"
+  "github.com/fzzy/radix/redis"
+  "github.com/jmoiron/sqlx"
 )
 
 var (
@@ -33,6 +37,11 @@ type (
     Value float32
     CreatedAt string
   }
+
+  Chunk struct {
+    Value int
+    CreatedAt time.Time
+  }
 )
 
 // ------- HELPERS --------
@@ -46,6 +55,131 @@ func renderTemplate(w http.ResponseWriter, tmpl string, ms []RootViewModel) {
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
   }
+}
+
+// Liefert alle Sensorennamen.
+func sensorMeasurementCount(sensor string) int64 {
+  // DB öffnen...
+  db, err := sqlx.Open("postgres", connectionString)
+  panicOnError(err)
+  // ...DB am Ende der Funktion wieder schließen.
+  defer db.Close()
+
+  // Letzten Eintrag abfragen.
+  var count int64
+  db.Select(&count, "SELECT count(*) FROM measurements WHERE sensor = $1", sensor)
+
+  return count
+}
+
+
+// Liefert alle Sensorennamen.
+func sensorNames() []string {
+  // DB öffnen...
+  db, err := sqlx.Open("postgres", connectionString)
+  panicOnError(err)
+  // ...DB am Ende der Funktion wieder schließen.
+  defer db.Close()
+
+  // Letzten Eintrag abfragen.
+  sensors := []string{}
+  db.Select(&sensors, "SELECT sensor FROM measurements")
+
+  return sensors
+}
+
+
+// Liefert alle Sensorennamen.
+func sensorMeasurementChunk(sensor string, chunkSize float64, offset float64) []Chunk {
+  // DB öffnen...
+  db, err := sqlx.Open("postgres", connectionString)
+  panicOnError(err)
+  // ...DB am Ende der Funktion wieder schließen.
+  defer db.Close()
+
+  // Letzten Eintrag abfragen.
+  chunks := []Chunk{}
+  db.Select(&chunks,
+    "SELECT * FROM measurements WHERE sensor = $1 LIMIT $2 OFFSET $3",
+    sensor, chunkSize, offset)
+
+  return chunks
+}
+
+func downsampleData(quit chan bool) {
+  const MAX_MEASUREMENTS = 500.0
+
+  log.Print("Starte Downsampler...")
+
+  // Verbindung zu Redis herstellen.
+  con, err := redis.DialTimeout("tcp", "127.0.0.1:6379", time.Duration(10)*time.Second)
+  if err != nil { log.Fatal(err) }
+  defer con.Close()
+
+  // Endlosschleife
+  for {
+    log.Print("Starte neuen Durchlauf...")
+
+    sensors := sensorNames()
+    log.Printf("%v", sensors)
+
+    for _,sensor := range sensors {
+      redisKey := "KEY_" + sensor // TODO
+      redisTmpKey := redisKey + ":" + string(time.Now().Unix())
+
+      count := float64(sensorMeasurementCount(sensor))
+      log.Printf("%s #%d", sensor, count)
+
+      chunkSize := math.Ceil(count / MAX_MEASUREMENTS)
+
+      for offset := 0.0; offset <= count; offset += chunkSize {
+        chunks := sensorMeasurementChunk(sensor, chunkSize, offset)
+
+        // Durchschntl. Temperature.
+        avgValue := 0
+        for _,c := range chunks { avgValue += c.Value }
+        avgValue /= len(chunks)
+
+        // Durchschnittliches Datum.
+        var avgUnixDate int64 = 0
+        for _,c := range chunks { avgUnixDate += c.CreatedAt.Unix() }
+        avgUnixDate /= int64(len(chunks))
+        avgDate := time.Unix(avgUnixDate, 0)
+
+        // Den Eintrag (also das "value" aus "key/value" in Redis) erstellen.
+        entry := "{'d':" + string(avgUnixDate) + "'v':" + string(avgValue) + "}"
+
+        // Neuen Eintrag in Redis speichern.
+        r := con.Cmd("zadd", redisTmpKey, avgDate, entry)
+        if r.Err != nil { log.Fatal(err) }
+
+      }
+
+      // Alte gegen neue Werte austauschen.
+      con.Cmd("multi")
+      con.Cmd("del", redisKey)
+      con.Cmd("rename", redisTmpKey, redisKey)
+      r := con.Cmd("exec")
+      if r.Err != nil { log.Fatal(err) }
+
+      log.Printf("Durchgang fertig!")
+
+    }
+
+    // Wir warten ganze x Sekunden vor dem nächsten Durchlauf.
+    log.Printf("Pause!")
+    time.Sleep(time.Second * 10)
+    // No-blocking: sollen wir beenden?
+    log.Printf("Beenden?")
+    select {
+      case <-quit: break
+      default:
+    }
+  }
+
+  // Wir sind fertig.
+  log.Printf("Beenden!")
+  quit <- true
 }
 
 // ------- HELPERS (END) ---
@@ -70,6 +204,21 @@ func main() {
     return postMeasurementHandler(mm)
   })
 
+
+  // Quit-Chanel: Hier schickt der Downsample
+  quit := make(chan bool)
+
+  go downsampleData(quit)
+
+
   log.Printf("Running on Port %d and using DB %s...", *port, *dbName)
   log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), m))
+
+  // Downsampler soll sich beenden.
+  log.Printf("Downsampler soll beenden...")
+  quit <- true
+  // Warten bis der Downsampler fertig ist.
+  log.Printf("Warten auf Downsampler...")
+  <-quit
+  log.Printf("FERTIG!")
 }
